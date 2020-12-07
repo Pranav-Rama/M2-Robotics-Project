b981e6eadafdbe24d431f77e22c834afabdae1e6
Izzul Azri <muhammadizzulazri@yahoo.com>
2020-12-07 13:04:27 +0100

    Updated Autonomous Waypoints

--- t3_waypoints/scripts/autonomous_waypoints.py
+++ t3_waypoints/scripts/autonomous_waypoints.py
@@ -9,7 +9,7 @@ from math import pow, sqrt
 
 class NavTest():
     def __init__(self):
-        rospy.init_node('nav_test', anonymous=True)
+        rospy.init_node('autonomous_way', anonymous=True)
         
         rospy.on_shutdown(self.shutdown)
         
@@ -31,18 +31,10 @@ class NavTest():
         # Pose coordinates are then displayed in the terminal
         # that was used to launch RViz.
         locations = dict()
-        
-        """locations['hall_foyer'] = Pose(Point(0.643, 4.720, 0.000), Quaternion(0.000, 0.000, 0.223, 0.975))
-        locations['hall_kitchen'] = Pose(Point(-1.994, 4.382, 0.000), Quaternion(0.000, 0.000, -0.670, 0.743))
-        locations['hall_bedroom'] = Pose(Point(-3.719, 4.401, 0.000), Quaternion(0.000, 0.000, 0.733, 0.680))
-        locations['living_room_1'] = Pose(Point(0.720, 2.229, 0.000), Quaternion(0.000, 0.000, 0.786, 0.618))
-        locations['living_room_2'] = Pose(Point(1.471, 1.007, 0.000), Quaternion(0.000, 0.000, 0.480, 0.877))
-        locations['dining_room_1'] = Pose(Point(-0.861, -0.019, 0.000), Quaternion(0.000, 0.000, 0.892, -0.451))"""
-
         #waypoints
-        locations['waypoint_1'] = Pose(Point(0.266694557546, 2.02686812866, 0.00), Quaternion(0.000, 0.000, -0.00387220340739, 0.999992502992))
-        locations['waypoint_2'] = Pose(Point(0.99999948937, -2.14577603054, 0.000), Quaternion(0.000, 0.000, -0.00149625181896, 0.999998880615))
-        locations['waypoint_3'] = Pose(Point(0.999998880615, 0.527852533338, 0.000), Quaternion(0.000, 0.000, -0.00992117199984, 0.999950783962))
+        locations['waypoint_1'] = Pose(Point(-9.48563145743, 0.993861001173, 0.00), Quaternion(0.000, 0.000, 0.0975400737238, 0.999992502992))
+        locations['waypoint_2'] = Pose(Point(8.78885627481, -3.8867439838, 0.000), Quaternion(0.000, 0.000, 0.705633593091, 0.70857690641))
+        locations['waypoint_3'] = Pose(Point(-12.4087143203, 0.260407563831, 0.000), Quaternion(0.000, 0.000, -0.999993994471, 0.0034656921885))
         
         # Publisher to manually control the robot (e.g. to stop it, queue_size=5)
         self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=5)
